b5ea4ca1d2c4824256320d2e15e85e88d2d1d719
9QIX <kirkhendrixbulaong@outlook.com>
2023-08-22 00:59:16 +0800

    PyPortScan Python File

--- PyPortScan.py
+++ PyPortScan.py
@@ -2,6 +2,7 @@ import socket
 import termcolor
 
 def scan(target, ports):
+    print('\n' + ' Starting Scan For ' + str(target))
     for port in range(1, ports):
         scan_port(target, port)
 
@@ -12,12 +13,13 @@ def scan_port(ipaddress, port):
         print("[+] Port Opened " + str(port))
         sock.close()
     except:
-        print("[-] Port Closed " + str(port))
+        #print("[-] Port Closed " + str(port))
+        pass
 
 target = input("[*] Enter Target To Scan(Split Them By Comma (,)): ")
 ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
 if ',' in target:
-    print("[*] Scanning Multiple Targets")
+    print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
     for ip_addr in target.split(','):
         scan(ip_addr.strip(' '), ports)
 else:
